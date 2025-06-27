2da5e90c63c4e5c81d91718959de1ab5b4903acd
Sam C. Finan <samcfinan@gmail.com>
2025-06-27 02:02:20 +0200

    Firecrawl but with Pitchright for bypassing detection

--- apps/playwright-service-ts/api.ts
+++ apps/playwright-service-ts/api.ts
@@ -1,9 +1,17 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import { chromium, Browser, BrowserContext, Route, Request as PlaywrightRequest, Page } from 'playwright';
-import dotenv from 'dotenv';
-import UserAgent from 'user-agents';
-import { getError } from './helpers/get_error';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import {
+  chromium,
+  Browser,
+  BrowserContext,
+  Route,
+  Request as PlaywrightRequest,
+  Page,
+  BrowserContextOptions,
+} from "patchright";
+import dotenv from "dotenv";
+import UserAgent from "user-agents";
+import { getError } from "./helpers/get_error";
 
 dotenv.config();
 
@@ -12,26 +20,27 @@ const port = process.env.PORT || 3003;
 
 app.use(bodyParser.json());
 
-const BLOCK_MEDIA = (process.env.BLOCK_MEDIA || 'False').toUpperCase() === 'TRUE';
+const BLOCK_MEDIA =
+  (process.env.BLOCK_MEDIA || "False").toUpperCase() === "TRUE";
 
 const PROXY_SERVER = process.env.PROXY_SERVER || null;
 const PROXY_USERNAME = process.env.PROXY_USERNAME || null;
 const PROXY_PASSWORD = process.env.PROXY_PASSWORD || null;
 
 const AD_SERVING_DOMAINS = [
-  'doubleclick.net',
-  'adservice.google.com',
-  'googlesyndication.com',
-  'googletagservices.com',
-  'googletagmanager.com',
-  'google-analytics.com',
-  'adsystem.com',
-  'adservice.com',
-  'adnxs.com',
-  'ads-twitter.com',
-  'facebook.net',
-  'fbcdn.net',
-  'amazon-adsystem.com'
+  "doubleclick.net",
+  "adservice.google.com",
+  "googlesyndication.com",
+  "googletagservices.com",
+  "googletagmanager.com",
+  "google-analytics.com",
+  "adsystem.com",
+  "adservice.com",
+  "adnxs.com",
+  "ads-twitter.com",
+  "facebook.net",
+  "fbcdn.net",
+  "amazon-adsystem.com",
 ];
 
 interface UrlModel {
@@ -47,23 +56,22 @@ let context: BrowserContext;
 
 const initializeBrowser = async () => {
   browser = await chromium.launch({
-    headless: true,
+    headless: false,
     args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-gpu'
-    ]
+      "--disable-dev-shm-usage",
+      "--no-first-run",
+      "--no-zygote",
+      "--disable-blink-features=AutomationControlled",
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--disable-gpu",
+    ],
   });
 
   const userAgent = new UserAgent().toString();
-  const viewport = { width: 1280, height: 800 };
+  const viewport = null;
 
-  const contextOptions: any = {
+  const contextOptions: BrowserContextOptions = {
     userAgent,
     viewport,
   };
@@ -83,17 +91,20 @@ const initializeBrowser = async () => {
   context = await browser.newContext(contextOptions);
 
   if (BLOCK_MEDIA) {
-    await context.route('**/*.{png,jpg,jpeg,gif,svg,mp3,mp4,avi,flac,ogg,wav,webm}', async (route: Route, request: PlaywrightRequest) => {
-      await route.abort();
-    });
+    await context.route(
+      "**/*.{png,jpg,jpeg,gif,svg,mp3,mp4,avi,flac,ogg,wav,webm}",
+      async (route: Route, request: PlaywrightRequest) => {
+        await route.abort();
+      }
+    );
   }
 
   // Intercept all requests to avoid loading ads
-  await context.route('**/*', (route: Route, request: PlaywrightRequest) => {
+  await context.route("**/*", (route: Route, request: PlaywrightRequest) => {
     const requestUrl = new URL(request.url());
     const hostname = requestUrl.hostname;
 
-    if (AD_SERVING_DOMAINS.some(domain => hostname.includes(domain))) {
+    if (AD_SERVING_DOMAINS.some((domain) => hostname.includes(domain))) {
       console.log(hostname);
       return route.abort();
     }
@@ -119,8 +130,17 @@ const isValidUrl = (urlString: string): boolean => {
   }
 };
 
-const scrapePage = async (page: Page, url: string, waitUntil: 'load' | 'networkidle', waitAfterLoad: number, timeout: number, checkSelector: string | undefined) => {
-  console.log(`Navigating to ${url} with waitUntil: ${waitUntil} and timeout: ${timeout}ms`);
+const scrapePage = async (
+  page: Page,
+  url: string,
+  waitUntil: "load" | "networkidle",
+  waitAfterLoad: number,
+  timeout: number,
+  checkSelector: string | undefined
+) => {
+  console.log(
+    `Navigating to ${url} with waitUntil: ${waitUntil} and timeout: ${timeout}ms`
+  );
   const response = await page.goto(url, { waitUntil, timeout });
 
   if (waitAfterLoad > 0) {
@@ -131,16 +151,22 @@ const scrapePage = async (page: Page, url: string, waitUntil: 'load' | 'networki
     try {
       await page.waitForSelector(checkSelector, { timeout });
     } catch (error) {
-      throw new Error('Required selector not found');
+      throw new Error("Required selector not found");
     }
   }
 
-  let headers = null, content = await page.content();
+  let headers = null,
+    content = await page.content();
   let ct: string | undefined = undefined;
   if (response) {
     headers = await response.allHeaders();
-    ct = Object.entries(headers).find(x => x[0].toLowerCase() === "content-type")?.[1];
-    if (ct && (ct[1].includes("application/json") || ct[1].includes("text/plain"))) {
+    ct = Object.entries(headers).find(
+      (x) => x[0].toLowerCase() === "content-type"
+    )?.[1];
+    if (
+      ct &&
+      (ct[1].includes("application/json") || ct[1].includes("text/plain"))
+    ) {
       content = (await response.body()).toString("utf8"); // TODO: determine real encoding
     }
   }
@@ -153,27 +179,35 @@ const scrapePage = async (page: Page, url: string, waitUntil: 'load' | 'networki
   };
 };
 
-app.post('/scrape', async (req: Request, res: Response) => {
-  const { url, wait_after_load = 0, timeout = 15000, headers, check_selector }: UrlModel = req.body;
+app.post("/scrape", async (req: Request, res: Response) => {
+  const {
+    url,
+    wait_after_load = 0,
+    timeout = 15000,
+    headers,
+    check_selector,
+  }: UrlModel = req.body;
 
   console.log(`================= Scrape Request =================`);
   console.log(`URL: ${url}`);
   console.log(`Wait After Load: ${wait_after_load}`);
   console.log(`Timeout: ${timeout}`);
-  console.log(`Headers: ${headers ? JSON.stringify(headers) : 'None'}`);
-  console.log(`Check Selector: ${check_selector ? check_selector : 'None'}`);
+  console.log(`Headers: ${headers ? JSON.stringify(headers) : "None"}`);
+  console.log(`Check Selector: ${check_selector ? check_selector : "None"}`);
   console.log(`==================================================`);
 
   if (!url) {
-    return res.status(400).json({ error: 'URL is required' });
+    return res.status(400).json({ error: "URL is required" });
   }
 
   if (!isValidUrl(url)) {
-    return res.status(400).json({ error: 'Invalid URL' });
+    return res.status(400).json({ error: "Invalid URL" });
   }
 
   if (!PROXY_SERVER) {
-    console.warn('⚠️ WARNING: No proxy server provided. Your IP address may be blocked.');
+    console.warn(
+      "⚠️ WARNING: No proxy server provided. Your IP address may be blocked."
+    );
   }
 
   if (!browser || !context) {
@@ -190,16 +224,34 @@ app.post('/scrape', async (req: Request, res: Response) => {
   let result: Awaited<ReturnType<typeof scrapePage>>;
   try {
     // Strategy 1: Normal
-    console.log('Attempting strategy 1: Normal load');
-    result = await scrapePage(page, url, 'load', wait_after_load, timeout, check_selector);
+    console.log("Attempting strategy 1: Normal load");
+    result = await scrapePage(
+      page,
+      url,
+      "load",
+      wait_after_load,
+      timeout,
+      check_selector
+    );
   } catch (error) {
-    console.log('Strategy 1 failed, attempting strategy 2: Wait until networkidle');
+    console.log(
+      "Strategy 1 failed, attempting strategy 2: Wait until networkidle"
+    );
     try {
       // Strategy 2: Wait until networkidle
-      result = await scrapePage(page, url, 'networkidle', wait_after_load, timeout, check_selector);
+      result = await scrapePage(
+        page,
+        url,
+        "networkidle",
+        wait_after_load,
+        timeout,
+        check_selector
+      );
     } catch (finalError) {
       await page.close();
-      return res.status(500).json({ error: 'An error occurred while fetching the page.' });
+      return res
+        .status(500)
+        .json({ error: "An error occurred while fetching the page." });
     }
   }
 
@@ -208,7 +260,9 @@ app.post('/scrape', async (req: Request, res: Response) => {
   if (!pageError) {
     console.log(`✅ Scrape successful!`);
   } else {
-    console.log(`🚨 Scrape failed with status code: ${result.status} ${pageError}`);
+    console.log(
+      `🚨 Scrape failed with status code: ${result.status} ${pageError}`
+    );
   }
 
   await page.close();
@@ -217,7 +271,7 @@ app.post('/scrape', async (req: Request, res: Response) => {
     content: result.content,
     pageStatusCode: result.status,
     contentType: result.contentType,
-    ...(pageError && { pageError })
+    ...(pageError && { pageError }),
   });
 });
 
@@ -227,9 +281,9 @@ app.listen(port, () => {
   });
 });
 
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   shutdownBrowser().then(() => {
-    console.log('Browser closed');
+    console.log("Browser closed");
     process.exit(0);
   });
 });
